37c3e80ede216f26a6fbd644bd7fbd732bd74bfc
shatha <73343178+shathalturke@users.noreply.github.com>
2020-11-26 08:48:48 -0800

    Update museum.php

--- museum.php
+++ museum.php
@@ -12,31 +12,28 @@
 
     <body>
 
-      <?php include 'goup.php';?>
-
-
         <div class="container">
 
 
             <nav class="navBar">
                 <ul class ="navList">
                     <li class="navItem">
-                        <a class="navLink" href="movies.html">الأفلام</a></li>
+                        <a class="navLink" href="movies.php">الأفلام</a></li>
                         <li class="navItem">
-                            <a class="navLink" href="oscar.html">الأوسكار</a></li>
+                            <a class="navLink" href="oscar.php">الأوسكار</a></li>
                     <li class="navItem">
-                            <a href="home.html"> <img src="Photos/logo.png" alt="websiteLogo" width="90" height="50"> </a></li>
+                            <a href="home.php"> <img src="Photos/logo.png" alt="websiteLogo" width="90" height="50"> </a></li>
                         <li class="navItem">
-                            <a class="navLink" href="museum.html">المتحف</a></li>
+                            <a class="navLink" href="museum.php">المتحف</a></li>
                             <li class="navItem">
-                                <a class="navLink" href="about.html">نبذة</a></li>
+                                <a class="navLink" href="about.php">نبذة</a></li>
                 </ul>
             </nav>
 
 
 
             <div class="mainSection">
-                <h3 class="path"><a class="home" href="home.html">الرئيسية</a> > المتحف</h3>
+                <h3 class="path"><a class="home" href="home.php">الرئيسية</a> > المتحف</h3>
                 <h1 class="title">متحف استديو جيبلي </h1>
 
 
