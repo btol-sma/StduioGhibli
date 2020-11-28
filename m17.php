de122c360ccd74195c7e7c1d54c5697942373d29
shatha <73343178+shathalturke@users.noreply.github.com>
2020-11-28 11:53:44 -0800

    Update m17.php

--- m17.php
+++ m17.php
@@ -42,7 +42,6 @@
         .youtube img{
           height: 1.6cm;
           width: 2cm;
-          float:center;
         }
 </style>
 </head>
@@ -70,7 +69,7 @@ Kiki's Delivery Service </h1>
 <div class=story>
 
 <div class=poster>
-<img src="photos\movies/f17.png" alt="كيكي لخدمة التوصيل">
+<img src="photos/movies/f17.png" alt="كيكي لخدمة التوصيل">
 <p><br><br><br></p>
 </div>
 
@@ -99,7 +98,7 @@ Kiki's Delivery Service </h1>
   </div>
   <div class=youtube>
      <a href="https://www.youtube.com/watch?v=4bG17OYs-GA" target="_blank">
-        <img src="photos\youtube.png" alt="إعلان الفيلم"></a>
+        <img src="photos/youtube.png" alt="إعلان الفيلم"></a>
 </div>
 </div>
 </div>
