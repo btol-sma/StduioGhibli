55989346dbd5fcc0f029a9bd045f256209026d90
shatha <73343178+shathalturke@users.noreply.github.com>
2020-11-28 11:48:35 -0800

    Update m10.php

--- m10.php
+++ m10.php
@@ -42,7 +42,6 @@
         .youtube img{
           height: 1.6cm;
           width: 2cm;
-          float:center;
         }
 </style>
 </head>
@@ -70,7 +69,7 @@ Ocean Waves</h1>
 <div class=story>
 
 <div class=poster>
-<img src="photos\movies/f10.png" alt="أمواج المحيط">
+<img src="photos/movies/f10.png" alt="أمواج المحيط">
 <p><br><br><br></p>
 </div>
 
@@ -100,7 +99,7 @@ Ocean Waves</h1>
   </div>
   <div class=youtube>
      <a href="https://www.youtube.com/watch?v=izyJ_NVyVaQ&feature=emb_title" target="_blank">
-        <img src="photos\youtube.png" alt="إعلان الفيلم"></a>
+        <img src="photos/youtube.png" alt="إعلان الفيلم"></a>
 </div>
 </div>
 </div>
